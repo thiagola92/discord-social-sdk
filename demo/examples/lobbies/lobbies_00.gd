c387455e4e279123d985ec178f3a244ca69cb41d
thiagola92 <thiagola92@gmail.com>
2026-06-15 12:35:22 -0300

    Update examples

--- demo/examples/lobbies/lobbies_00.gd
+++ demo/examples/lobbies/lobbies_00.gd
@@ -7,15 +7,97 @@ var application_id: int = DotEnv.read_int("APPLICATION_ID")
 
 var client := DiscordClient.new()
 
+var code_verifier: DiscordAuthorizationCodeVerifier
+
 
 func _ready() -> void:
+	var args := DiscordAuthorizationArgs.new()
+	code_verifier = client.create_authorization_code_verifier()
+	
+	args.set_scopes(DiscordClient.get_default_communication_scopes())
+	args.set_code_challenge(code_verifier.challenge())
+	
 	client.set_application_id(application_id)
-	client.create_or_join_lobby("your-unique-lobby-secret", _on_joined_lobby)
+	client.add_log_callback(_on_log, DiscordLoggingSeverity.INFO)
+	client.set_status_changed_callback(_on_status_changed)
+	client.authorize(args, _on_authorization_response)
 
 
 func _process(_delta: float) -> void:
 	Discord.run_callbacks()
 
 
+func _on_log(message: String, severity: DiscordLoggingSeverity.Enum) -> void:
+	var enum_str: String = Discord.enum_to_string(severity, DiscordLoggingSeverity.id)
+	
+	print("[%s] %s" % [enum_str, message])
+
+
+func _on_status_changed(status: DiscordClientStatus.Enum, _error: DiscordClientError.Enum, _error_detail: int) -> void:
+	var enum_str: String = Discord.enum_to_string(status, DiscordClientStatus.id)
+	
+	print("Status changed to %s" % enum_str)
+	
+	if status == DiscordClientStatus.READY:
+		client.create_or_join_lobby("your-unique-lobby-secret", _on_joined_lobby)
+
+
 func _on_joined_lobby(result: DiscordClientResult, lobby_id: int) -> void:
-	pass
+	if result.successful():
+		print("🎮 Lobby created or joined successfully! Lobby Id: %s" % lobby_id)
+		
+		client.send_lobby_message(lobby_id, "Hello", _on_lobby_message)
+		
+		await get_tree().create_timer(60).timeout
+		
+		client.leave_lobby(lobby_id, _on_left_lobby)
+	else:
+		print("❌ Lobby creation/join failed")
+
+
+func _on_lobby_message(result: DiscordClientResult, message_id: int) -> void:
+	if result.successful():
+		print("📨 Message sent successfully! Message Id: %s" % message_id)
+	else:
+		print("❌ Message sending failed")
+
+
+func _on_left_lobby(result: DiscordClientResult) -> void:
+	if result.successful():
+		print("🎮 Left lobby successfully!")
+	else:
+		print("❌ Leaving lobby failed")
+
+
+func _on_authorization_response(result: DiscordClientResult, code: String, redirect_uri: String) -> void:
+	if not result.successful():
+		print("❌ Authorization Error: %s" % result.error())
+		return
+	
+	print("✅ Authorization successful! Next step: exchange code for an access token")
+	client.get_token(application_id, code, code_verifier.verifier(), redirect_uri, _on_token_received)
+
+
+func _on_token_received(
+	result: DiscordClientResult,
+	access_token: String,
+	_refresh_token: String,
+	token_type: DiscordAuthorizationTokenType.Enum,
+	_expires_in: int,
+	_scopes: String
+) -> void:
+	if not result.successful():
+		print("❌ Token Error: %s" % result.error())
+		return
+	
+	print("🔓 Access token received! Establishing connection...")
+	client.update_token(token_type, access_token, _on_token_updated)
+
+
+func _on_token_updated(result: DiscordClientResult) -> void:
+	if not result.successful():
+		print("❌ Token Update Error: %s" % result.error())
+		return
+	
+	print("🔑 Token updated, connecting to Discord...")
+	client.connect_discord()
--- demo/examples/lobbies/lobbies_01.gd
+++ demo/examples/lobbies/lobbies_01.gd
@@ -1,11 +1,102 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
+# ATTENTION: Replace DotEnv.read_int("APPLICATION_ID") with your application ID.
+# This only exist so I don't accidentally git push my ID.
+var application_id: int = DotEnv.read_int("APPLICATION_ID")
+
+var client := DiscordClient.new()
+
+var code_verifier: DiscordAuthorizationCodeVerifier
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	var args := DiscordAuthorizationArgs.new()
+	code_verifier = client.create_authorization_code_verifier()
+	
+	args.set_scopes(DiscordClient.get_default_communication_scopes())
+	args.set_code_challenge(code_verifier.challenge())
+	
+	client.set_application_id(application_id)
+	client.add_log_callback(_on_log, DiscordLoggingSeverity.INFO)
+	client.set_status_changed_callback(_on_status_changed)
+	client.set_message_created_callback(_on_message_created)
+	client.authorize(args, _on_authorization_response)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	Discord.run_callbacks()
+
+
+func _on_log(message: String, severity: DiscordLoggingSeverity.Enum) -> void:
+	var enum_str: String = Discord.enum_to_string(severity, DiscordLoggingSeverity.id)
+	
+	print("[%s] %s" % [enum_str, message])
+
+
+func _on_status_changed(status: DiscordClientStatus.Enum, _error: DiscordClientError.Enum, _error_detail: int) -> void:
+	var enum_str: String = Discord.enum_to_string(status, DiscordClientStatus.id)
+	
+	print("Status changed to %s" % enum_str)
+	
+	if status == DiscordClientStatus.READY:
+		client.create_or_join_lobby("your-unique-lobby-secret", _on_joined_lobby)
+
+
+func _on_joined_lobby(result: DiscordClientResult, lobby_id: int) -> void:
+	if result.successful():
+		print("🎮 Lobby created or joined successfully! Lobby Id: %s" % lobby_id)
+		
+		await get_tree().create_timer(60).timeout
+		
+		client.leave_lobby(lobby_id, _on_left_lobby)
+	else:
+		print("❌ Lobby creation/join failed")
+
+
+func _on_left_lobby(result: DiscordClientResult) -> void:
+	if result.successful():
+		print("🎮 Left lobby successfully!")
+	else:
+		print("❌ Leaving lobby failed")
+
+
+func _on_message_created(message_id: int) -> void:
+	var message = client.get_message_handle(message_id)
+	
+	if message is DiscordMessageHandle:
+		print("📨 New message received: " % message.content())
+
+
+func _on_authorization_response(result: DiscordClientResult, code: String, redirect_uri: String) -> void:
+	if not result.successful():
+		print("❌ Authorization Error: %s" % result.error())
+		return
+	
+	print("✅ Authorization successful! Next step: exchange code for an access token")
+	client.get_token(application_id, code, code_verifier.verifier(), redirect_uri, _on_token_received)
+
+
+func _on_token_received(
+	result: DiscordClientResult,
+	access_token: String,
+	_refresh_token: String,
+	token_type: DiscordAuthorizationTokenType.Enum,
+	_expires_in: int,
+	_scopes: String
+) -> void:
+	if not result.successful():
+		print("❌ Token Error: %s" % result.error())
+		return
+	
+	print("🔓 Access token received! Establishing connection...")
+	client.update_token(token_type, access_token, _on_token_updated)
+
+
+func _on_token_updated(result: DiscordClientResult) -> void:
+	if not result.successful():
+		print("❌ Token Update Error: %s" % result.error())
+		return
+	
+	print("🔑 Token updated, connecting to Discord...")
+	client.connect_discord()
