a1118ef4c9edf9497108116d4b1cbd2309fca8b4
thiagola92 <thiagola92@gmail.com>
2026-09-23 20:31:39 -0300

    Update example to show that changing incoming audio works

--- demo/examples/voice/voice_01.gd
+++ demo/examples/voice/voice_01.gd
@@ -60,10 +60,9 @@ func _on_joined_lobby(result: DiscordClientResult, lobby_id: int) -> void:
 		print("❌ Failed to join lobby: %s" % result.error())
 
 
-func _on_audio_received(user_id: int, data: Array[int], samples_per_channel: int, sample_rate: int, channels: int, out_should_mute: bool) -> void:
-	# Changing "data" doesn't reflect into SDK.
+func _on_audio_received(user_id: int, data: DiscordInt16Array, samples_per_channel: int, sample_rate: int, channels: int, out_should_mute: bool) -> void:
 	for i in data.size():
-		data[i] *= 0.5
+		data.set(i, data.get(i) * 0.1)
 	
 	# Changing "out_should_mute" doesn't reflect into SDK.
 	out_should_mute = true
@@ -71,7 +70,7 @@ func _on_audio_received(user_id: int, data: Array[int], samples_per_channel: int
 	var total_num_samples = samples_per_channel * channels
 
 
-func _on_audio_captured(data: Array[int], samples_per_channel: int, sample_rate: int, channels: int) -> void:
+func _on_audio_captured(data: DiscordInt16Array, samples_per_channel: int, sample_rate: int, channels: int) -> void:
 	pass
 
 
